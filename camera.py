3e6a47955883ae3f30e342513c8c58857eef4105
eat-sleep-code <info@eat-sleep-code.com>
2021-03-07 17:23:07 -0700

    Fix hint

--- camera.py
+++ camera.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 import tkinter as tk
 from tkinter import ttk
 
--- controls.py
+++ controls.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
